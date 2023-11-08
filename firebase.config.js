3a20bb1d3d2a3fc8f890c1c61809fc860878c051
jkminhaj <mmminhaj221@gmail.com>
2023-11-08 14:57:23 +0600

    firebase info is hidden now

--- firebase.config.js
+++ firebase.config.js
@@ -7,12 +7,12 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCemO3JKQhH6IiEr_ulpAFQBbCMceBasXs",
-    authDomain: "job2324-7cf51.firebaseapp.com",
-    projectId: "job2324-7cf51",
-    storageBucket: "job2324-7cf51.appspot.com",
-    messagingSenderId: "961864999700",
-    appId: "1:961864999700:web:062bc85ab6593e19fe1801"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };  
 
 // Initialize Firebase
